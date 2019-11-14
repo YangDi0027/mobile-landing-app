14a2467eef105ab8450c1b4d93f20690343fd4b5
YangDi0027 <yangdi0027@outlook.com>
2019-11-14 08:59:39 +0800

    build update

---
+++ build/precache-manifest.9e9c1b75dde71774479ca2bd94a0118c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f05a051b28ae6bc316e62b4cd4f4865",
+    "url": "/mobile-landing-app/index.html"
+  },
+  {
+    "revision": "55e8c871eb5d8cf6d3a8",
+    "url": "/mobile-landing-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6e25a06c3596a3d61997",
+    "url": "/mobile-landing-app/static/css/main.2312f766.chunk.css"
+  },
+  {
+    "revision": "55e8c871eb5d8cf6d3a8",
+    "url": "/mobile-landing-app/static/js/2.54225aef.chunk.js"
+  },
+  {
+    "revision": "6e25a06c3596a3d61997",
+    "url": "/mobile-landing-app/static/js/main.2b347035.chunk.js"
+  },
+  {
+    "revision": "7a2d0057edfce494a3b6",
+    "url": "/mobile-landing-app/static/js/runtime-main.2b4c73e5.js"
+  },
+  {
+    "revision": "deca5a51f523e1051b521f0c83abe357",
+    "url": "/mobile-landing-app/static/media/ACS.deca5a51.png"
+  },
+  {
+    "revision": "1ba63fab9c1281186157ed0931200f83",
+    "url": "/mobile-landing-app/static/media/Royal_s.1ba63fab.png"
+  },
+  {
+    "revision": "971a80bff376a402a6edd5c0a5b505ac",
+    "url": "/mobile-landing-app/static/media/burn_en_us_set_photo_original.971a80bf.png"
+  },
+  {
+    "revision": "3c03f83b50ac41fd270ff1502d0dda92",
+    "url": "/mobile-landing-app/static/media/colors_v_2_en_us_set_photo_original.3c03f83b.png"
+  },
+  {
+    "revision": "9ed940d02a073f3745288c8846bc4bcd",
+    "url": "/mobile-landing-app/static/media/contents_image_bto_3_lj.9ed940d0.png"
+  },
+  {
+    "revision": "c7094547a1f2a3349a660697e3e47628",
+    "url": "/mobile-landing-app/static/media/contents_image_neqlof.c7094547.png"
+  },
+  {
+    "revision": "7a644376ea3a713ec56ad9c623b6b1a6",
+    "url": "/mobile-landing-app/static/media/contents_image_xuhh_1_l.7a644376.png"
+  },
+  {
+    "revision": "5d43e6728fe1cae9e0dc47457502f242",
+    "url": "/mobile-landing-app/static/media/free_vr.5d43e672.png"
+  },
+  {
+    "revision": "2e029a82fdf4c88ab74a82d77026a62a",
+    "url": "/mobile-landing-app/static/media/garther.2e029a82.png"
+  },
+  {
+    "revision": "cf6dfe89dd99d33adf1f52c73c9f4cf3",
+    "url": "/mobile-landing-app/static/media/live_science.cf6dfe89.png"
+  },
+  {
+    "revision": "8714015abe6aecfdf8b47680b9871f55",
+    "url": "/mobile-landing-app/static/media/together.8714015a.png"
+  }
+]);
